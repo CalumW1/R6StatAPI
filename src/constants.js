8e2c455d22bd30eb54d57b37e412287c86b2edb4
Calum Wilson <49264699+CalumW1@users.noreply.github.com>
2023-04-30 21:44:18 +1000

    getuserbyusername method

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,5 @@ export const BASE_UBI_URI = version => `https://public-ubiservices.ubi.com/v${ve
 export const UBI_APPID = '3587dcbb-7f81-457c-9781-0e3f29f6f56a';
 
 export const UBI_AUTH_URI = '/profiles/sessions';
+
+export const UBI_GETUSERBYUSERNAME_URI = (userName, platform) => `/profiles?namesOnPlatform=${userName}&platformType=${platform}`
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import getAuth from './auth.js';
+import getUserByUsername from './getUserByUsername.js';
 
 const email = 'calumwilson2000@gmail.com';
 const password = 'Trivium19-';
 
-setInterval(async () => {
-    const token = await getAuth(email, password);
-    console.log(token);
-}, 60000)
+// setInterval(async () => {
+//     const token = await getAuth(email, password);
+//     console.log(token);
+// }, 60000)
+
+const token = await getAuth(email, password);
+console.log(token);
+
+const player = await getUserByUsername("CaleyW1", "uplay");
+console.log(player);
