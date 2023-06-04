2f1cdeed7eab9a8a7382c2083d6f646a5976ceb8
Calum Wilson <wilsoncalum12@gmail.com>
2023-06-04 20:31:17 +1000

    Classes update 2

--- src/getUserRankV1.js
+++ src/getUserRankV1.js
@@ -7,6 +7,7 @@ import {
   UBI_SANDBOXES,
 } from './constants.js';
 import getAuth from './auth.js';
+import { UserRankDtoV1 } from './constants.js';
 
 const getUserRankV1 = async (platform, boardId, regionId, profileId) => {
   const token = await getAuth();
@@ -31,6 +32,45 @@ const getUserRankV1 = async (platform, boardId, regionId, profileId) => {
   });
 
   const data = await response.json();
+
+  const test = data.seasons_player_skill_records.flatMap(season => {
+    return season.regions_player_skill_records.flatMap(region => {
+      return region.boards_player_skill_records.flatMap(board => {
+        return board.players_skill_records.flatMap(player => {
+          return new UserRankDtoV1(
+            player.max_mmr,
+            player.skill_mean,
+            player.deaths,
+            player.profile_id,
+            player.next_rank_mmr,
+            player.rank,
+            player.max_rank,
+            player.board_id,
+            player.skill_stdev,
+            player.kills,
+            player.last_match_skill_stdev_change,
+            player.past_seasons_wins,
+            player.update_time,
+            player.last_match_mmr_change,
+            player.abandons,
+            player.season,
+            player.past_seasons_losses,
+            player.top_rank_position,
+            player.last_match_skill_mean_change,
+            player.mmr,
+            player.previous_rank_mmr,
+            player.last_match_result,
+            player.past_seasons_abandons,
+            player.wins,
+            player.region,
+            player.losses
+          );
+        });
+      });
+    });
+  });
+
+  console.log(test);
 
   return data;
 };
