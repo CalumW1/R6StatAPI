e4447eaf2f3faa2cb100a5d4a07a42ca3c577e29
Calum Wilson <wilsoncalum12@gmail.com>
2024-03-29 09:33:26 +1000

    added get user by userId

---
+++ src/infrastructure/GetUserByUserIdQueryHandler.js
@@ -0,0 +1,29 @@
+import { AuthCommandHandler } from './AuthCommandHandler.js';
+import { ApiClient } from './ApiClient.js';
+import { UserByUserIdDto } from '../domain/entitites/UserByUserIdDto.js';
+import { UBI_APPID, UBI_GETUSERBYID_URI, BASE_UBI_URI } from '../utils/helperFunctions.js';
+
+export const GetUserByUserIdQueryHandler = async userId => {
+  const token = await AuthCommandHandler();
+
+  const headers = {
+    Authorization: `ubi_v1 t=${token}`,
+    'Ubi-AppId': UBI_APPID,
+    'Content-Type': 'application/json',
+  };
+
+  const URI = BASE_UBI_URI(3) + UBI_GETUSERBYID_URI(userId);
+
+  const data = await ApiClient(URI, headers, 'GET');
+
+  return data.profiles.reduce((accumulator, x) => {
+    accumulator.push({
+      profileId: x.profileId,
+      userId: x.userId,
+      platformType: x.platformType,
+      idOnPlatform: x.idOnPlatform,
+      nameOnPlatform: x.nameOnPlatform,
+    });
+    return accumulator;
+  }, []);
+};
