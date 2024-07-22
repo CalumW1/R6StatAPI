8fdf0e9bd2d05b839e5a617f154a02790ddbdc79
Calum Wilson <wilsoncalum12@gmail.com>
2024-07-22 21:29:54 +1000

    updated auth command

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Auth, Authorise } from './methods/auth';
 
 export class R6StatAPI {
-  public async Login(email: string, password: string): Promise<Authorise> {
+  public async Login(email: string, password: string): Promise<string> {
     return Auth(email, password);
   }
 }
--- src/methods/auth.ts
+++ src/methods/auth.ts
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { UBI_APPID, BASE_UBI_URI, UBI_AUTH_URI } from '../constants';
-import fs from 'fs/promises';
+import { writeFile } from 'fs';
 
 export interface Authorise {
   platformType: string;
@@ -20,7 +20,34 @@ export interface Authorise {
   rememberMeTicket: string;
 }
 
-export const Auth = async (email: string, password: string): Promise<Authorise> => {
+const FileName = 'Auth.json';
+
+let Email: string = '';
+let Password: string = '';
+let Token: string = '';
+let NextRefresh: string = '';
+let Experation: string = '';
+
+export const Auth = async (email: string, password: string): Promise<string> => {
+  const currentTime = new Date().toISOString();
+
+  if (Token !== null && currentTime < NextRefresh) {
+    return Token;
+  }
+
+  if (email && password !== undefined) {
+    Email = email;
+    Password = password;
+  }
+
+  const newToken = await RequestToken(Email, Password);
+
+  return newToken.ticket;
+};
+
+const RequestToken = async (email: string, password: string): Promise<Authorise> => {
+  console.log('Refreshing Token...');
+
   const headers = {
     Authorization: `Basic ${Buffer.from(`${email}:${password}`).toString('base64')}`,
     'Ubi-AppId': UBI_APPID,
@@ -37,6 +64,18 @@ export const Auth = async (email: string, password: string): Promise<Authorise> 
   if (!response.ok) throw Error('Login was not successful');
 
   const data = (await response.json()) as Authorise;
+
+  Token = data.ticket;
+  NextRefresh = data.experation;
+  Experation = data.experation;
+
+  writeFile(FileName, JSON.stringify(data, null, 2), err => {
+    if (err) {
+      console.error('Error writing to file', err);
+    } else {
+      console.log('Token data has been saved to file!');
+    }
+  });
 
   return data;
 };
