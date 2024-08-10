1b54783ce23d095a024cf9d387b09f348da4eb91
Calum Wilson <wilsoncalum12@gmail.com>
2024-08-11 03:28:12 +1000

    added get operator

--- src/methods/GetOperator.ts
+++ src/methods/GetOperator.ts
@@ -17,8 +17,6 @@ interface OperatorStats {
   type: string;
   statsType: string;
   statsDetail: string;
-  seasonYear: string;
-  seasonNumber: string;
   matchesPlayed: number;
   roundsPlayed: number;
   minutesPlayed: number;
@@ -42,9 +40,8 @@ interface OperatorStats {
   KillDeathRatio: number;
   headshotAccuracy: number;
   killsPerRound: number;
-  roundsWithAkull: number;
   roundsWithAKill: number;
-  roundsWithAMultiKill: number;
+  roundsWithMultiKill: number;
   roundsWithOpeningKill: number;
   roundsWithOpeningDeath: number;
   roundsWithKOST: number;
@@ -80,21 +77,93 @@ export const GetOperator = async (
 
   const spaceId: any = RANKED_UBI_SPACEIDS.find(x => x.id === platformChange)?.value;
 
-  console.log(`Experation token ${expiration}`);
-  console.log(`spaceId ${spaceId}`);
-
   const URI =
     UBI_DATADEV_URI +
     UBI_GETSTATS(userId, spaceId, platformChange, view, aggregation, gameMode, teamRole, season);
 
   const data = await ApiClient(URI, headers, 'GET');
 
-  console.log(JSON.stringify(data));
+  const operators: Operators = await ExtractOperators(await data.json(), userId);
 
+  return operators;
+};
+
+const ExtractOperators = async (data: any, userId: string): Promise<Operators> => {
   const operators: Operators = {
     attackers: [],
     defenders: [],
   };
 
+  const profile = data.profileData[`${userId}`].platforms.PC.gameModes.ranked.teamRoles;
+  const attackers = profile.attacker;
+  const defenders = profile.defender;
+
+  console.log(`Attackers count: ${attackers.length}, Defenders count: ${defenders.length}`);
+
+  if (attackers.length > 0) {
+    const attackersPromise = attackers.map(async (operator: any) => {
+      const operatorStats: OperatorStats = await BuildOperator(operator);
+      return operatorStats;
+    });
+
+    const resolvedAttackers = await Promise.all(attackersPromise);
+    operators.attackers.push(...resolvedAttackers);
+  }
+
+  if (defenders.length > 0) {
+    const defendersPromise = defenders.map(async (operator: any) => {
+      const operatorStats: OperatorStats = await BuildOperator(operator);
+      return operatorStats;
+    });
+
+    const resolvedDefender = await Promise.all(defendersPromise);
+    operators.defenders.push(...resolvedDefender);
+  }
+
+  // console.log(`Attackers length: ${operators.attackers.length}`);
+  // console.log(`Defenders length: ${operators.defenders.length}`);
   return operators;
+};
+
+const BuildOperator = async (operator: any): Promise<OperatorStats> => {
+  const stats: OperatorStats = {
+    type: operator.type,
+    statsType: operator.statsType,
+    statsDetail: operator.statsDetail,
+    matchesPlayed: operator.matchesPlayed,
+    roundsPlayed: operator.roundsPlayed,
+    minutesPlayed: operator.minutesPlayed,
+    matchesWon: operator.matchesWon,
+    matchesLost: operator.matchesLost,
+    roundsWon: operator.roundsWon,
+    roundsLost: operator.roundsLost,
+    kills: operator.kills,
+    assists: operator.assists,
+    deaths: operator.death,
+    headshots: operator.headshots,
+    meleeKills: operator.meleeKills,
+    teamKills: operator.teamKills,
+    openingKills: operator.openingKills,
+    trades: operator.trades,
+    openingKillTrades: operator.openingKillTrades,
+    openingDeathTrades: operator.openingDeathTrades,
+    revives: operator.revives,
+    distanceTravelled: operator.distanceTravelled,
+    winLossRatio: operator.winLossRatio,
+    KillDeathRatio: operator.killDeathRatio.value,
+    headshotAccuracy: operator.headshotAccuracy.value,
+    killsPerRound: operator.killsPerRound.value,
+    roundsWithAKill: operator.killsPerRound.value,
+    roundsWithMultiKill: operator.roundsWithMultiKill.value,
+    roundsWithOpeningKill: operator.roundsWithOpeningKill.value,
+    roundsWithOpeningDeath: operator.roundsWithOpeningDeath.value,
+    roundsWithKOST: operator.roundsWithKOST.value,
+    roundsSurvived: operator.roundsSurvived.value,
+    roundsWithAnAce: operator.roundsWithAnAce.value,
+    roundsWithClutch: operator.roundsWithClutch.value,
+    timeAlivePerMatch: operator.timeAlivePerMatch,
+    timeDeadPerMatch: operator.timeDeadPerMatch,
+    distancePerRound: operator.distancePerRound,
+  };
+  return stats;
 };
