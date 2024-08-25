caad9d44b1c428114ee82645538620327f202934
Fabian <info@maierfabian.de>
2024-08-26 00:35:43 +0200

    made it compatible with bun

--- src/methods/apiClient.ts
+++ src/methods/apiClient.ts
@@ -1,12 +1,12 @@
-import fetch from 'node-fetch';
-
 export const ApiClient = async (url: string, header: any, method: string): Promise<any> => {
   const response = await fetch(url, {
     method: method,
     headers: header,
   });
 
-  if (!response.ok) throw Error('Error with response');
+  if (!response.ok) {
+    throw new Error('Error with response');
+  }
 
   return response;
 };
